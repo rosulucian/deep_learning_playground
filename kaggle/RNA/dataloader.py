4d3cc625fdf3e6140092ac936e10791a84ac27cb
rosulucian <rosu.lucian@gmail.com>
2023-11-15 16:58:55 +0200

    added nsp task in new notebook
    also updated dataset class to support nsp task

--- kaggle/RNA/dataloader.py
+++ kaggle/RNA/dataloader.py
@@ -1,29 +1,58 @@
-import numpy as np
 import torch
+import numpy as np
 
 class RNA_Dataset(torch.utils.data.Dataset):
     def __init__(self, df, Lmax, v, seed,  mode='train',
-                 mask_only=False, **kwargs):
+                 nsp=False, sep='S', **kwargs):
         
         self.seq_map = v
-        self.Lmax = Lmax
+        self.Lmax = Lmax + 1
         
         self.seq = df['sequence'].values
         self.L = df['L'].values
         
-        self.mask_only = mask_only
+        self.nsp = nsp
+        self.sep = sep
         
     def __len__(self):
-        return len(self.seq)  
+        return len(self.seq)
     
+    
+    def get_nsp(self, seq):
+        sep_idx = len(seq) // 2
+        
+        # isnext = True
+        nsp_target = [1, 0]
+        
+        # get another rand seq
+        if torch.rand(1) < 0.5:
+            # isnext = False
+            nsp_target = [0, 1]
+            
+            idx = np.random.choice(len(self.seq))
+            other = self.seq[idx]
+
+            max_len = max(len(seq), len(other))
+            sep_idx = max_len // 2
+            
+            nsp = seq[:sep_idx] + self.sep + other[sep_idx:]
+        else:
+            nsp = seq[:sep_idx] + self.sep + seq[sep_idx:]
+        
+        return nsp, sep_idx, torch.Tensor(nsp_target)
+        
     def __getitem__(self, idx):
         seq = self.seq[idx]
+        
+        if self.nsp:
+            seq, sep_idx, nsp_target = self.get_nsp(seq)
         
         mask = torch.zeros(self.Lmax, dtype=torch.bool)
         mask[:len(seq)] = True
         
-        if self.mask_only:
-            return {'mask':mask}
+        # if len(seq) >= self.Lmax:
+        #     print(len(seq))
+        #     print(seq)
         
         seq = [self.seq_map[s] for s in seq]
         seq = np.array(seq)        
@@ -43,11 +72,17 @@ class RNA_Dataset(torch.utils.data.Dataset):
         # true when token is masked 
         token_mask = mlm == self.seq_map['M']
         
-        #        
+        # set all to 0 except mask
         mlm_target = seq.masked_fill(~token_mask, 0)
         
+        inputs = {'seq': mlm, 'att_mask': mask}
+        targets = {'labels': seq, 'token_mask': token_mask, 'mlm_target': mlm_target.long()}
         
-        return {'seq': mlm, 'att_mask': mask}, {'labels': seq, 'token_mask': token_mask, 'mlm_target': mlm_target.long()}
+        if self.nsp:
+            mlm[sep_idx] = self.seq_map['S']
+            targets["nsp_target"] = nsp_target
+        
+        return inputs, targets
 
 def create_dataloader(train_ds, batch_size=2, num_workers=0, persistent_workers=True, drop_last=True):
     
@@ -56,11 +91,18 @@ def create_dataloader(train_ds, batch_size=2, num_workers=0, persistent_workers=
         batch_size=batch_size, 
         num_workers=num_workers, 
         persistent_workers=persistent_workers, 
-        drop_last=drop_last
+        drop_last=drop_last,
     )
     
     return loader
 
 class DataloaderWrapper:
     def __init__(self, train_ds, batch_size=2, num_workers=0, persistent_workers=True, drop_last=True, shuffle=False):
-        self.dataloader = torch.utils.data.DataLoader(dataset=train_ds, batch_size=batch_size, persistent_workers=persistent_workers, shuffle=shuffle, num_workers=num_workers, drop_last=drop_last)
+        self.dataloader = torch.utils.data.DataLoader(
+            dataset=train_ds, 
+            batch_size=batch_size, 
+            persistent_workers=persistent_workers, 
+            shuffle=shuffle, 
+            num_workers=num_workers, 
+            drop_last=drop_last,
+        )
