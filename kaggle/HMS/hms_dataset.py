fe53d786a9299f301c82795ebf10bdfca4955c05
rosulucian <rosu.lucian@gmail.com>
2024-03-04 13:15:59 +0200

    added normalization to dataset

--- kaggle/HMS/hms_dataset.py
+++ kaggle/HMS/hms_dataset.py
@@ -9,21 +9,23 @@ from albumentations.pytorch import ToTensorV2
 # img_mean=[0.485, 0.456, 0.406]
 # img_std=[0.229, 0.224, 0.225]
 
-img_mean=[0.485]
-img_std=[0.229]
+mean = np.array([-0.22308692079693776, -0.23225270031972337, -0.2646080688103756, -0.27772951886156666], dtype=np.float32)
+std = np.array([2.4021079842036257, 2.3784709900060506, 2.4214762588834593, 2.366489507911308], dtype=np.float32)
 
-norm_tf = A.Normalize(mean=img_mean, std=img_std, p=1.0)
+# norm_tf = A.Normalize(mean=img_mean, std=img_std, p=1.0)
 
 class spectro_dataset(torch.utils.data.Dataset):
-    def __init__(self, df, base_dir, cols):
+    def __init__(self, df, base_dir, cols, normalize=True):
         super().__init__()
         
         self.df = df
         self.cols = cols
         self.len = len(self.df)
         self.base_dir = base_dir
+
+        self.normalize = normalize
         
-        self.norm_tf = norm_tf
+        # self.norm_tf = norm_tf
         
     def __len__(self):
         return self.len
@@ -37,18 +39,28 @@ class spectro_dataset(torch.utils.data.Dataset):
         if offset%2==0: offset += 1
         
         spectro = pd.read_parquet(f'{self.base_dir}/{entry_id}.parquet')
+        # 300x400
         spectro = spectro.loc[(spectro.time >= offset)&(spectro.time <= offset+598)]
         spectro = spectro.iloc[:,1:]
-
+        # 400x300
         spectro = spectro.to_numpy().T
 #         spectro = spectro.reshape(4,100,-1)
-        
+
         spectro = np.nan_to_num(spectro, copy=True, nan=0.0)
-        # spectro = self.norm_tf(image=spectro)['image']
+        np.clip(spectro,np.exp(-4),np.exp(8), out=spectro)
+        np.log(spectro, out=spectro)
+
+        # 4X100X300
+        spectro = spectro.reshape(4,100,-1)
         
-        spectro = ToTensorV2(p=1.0)(image=spectro)['image'].reshape(4,100,-1)
-        # spectro = self.norm_tf(image=spectro)['image']
+        if self.normalize is True:
+            denom = np.reciprocal(std, dtype=np.float32)
+            spectro = spectro.T - mean
+            spectro *= denom
+            
+            spectro = spectro.T
 
         target = entry[self.cols].div(entry['total_votes'], axis=0).to_numpy(dtype=np.float32)
         
-        return spectro, target
+        return torch.from_numpy(spectro), target
+
