25bac3fc9e2f5ae1fa920ed0bd5529043049a29c
rosulucian <rosu.lucian@gmail.com>
2024-04-30 13:35:36 +0300

    add get_pinecone_index to dlaiutils

--- projects/DLAIUtils.py
+++ projects/DLAIUtils.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+from pinecone import Pinecone, ServerlessSpec
 from dotenv import load_dotenv, find_dotenv
 
 
@@ -26,3 +28,16 @@ class Utils:
     def get_pinecone_api_key(self):
         _ = load_dotenv(find_dotenv())
         return os.getenv("PINECONE_API_KEY")
+
+    def get_pinecone_index(self, INDEX_NAME, pinecone, dimension=1536, metric='cosine'):
+        
+        if INDEX_NAME in [index.name for index in pinecone.list_indexes()]:
+            pinecone.delete_index(INDEX_NAME)
+        
+        pinecone.create_index(
+            name=INDEX_NAME, 
+            dimension=dimension, 
+            metric=metric,
+            spec=ServerlessSpec(cloud='aws', region='us-east-1'))
+        
+        return pinecone.Index(INDEX_NAME)
