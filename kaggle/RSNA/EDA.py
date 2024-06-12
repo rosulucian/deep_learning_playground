676b79e2ab27ea68c2d4da1a96c8c1b691e6d52a
rosulucian <rosu.lucian@gmail.com>
2024-06-12 16:04:32 +0300

    RSNA first

---
+++ kaggle/RSNA/EDA.py
@@ -0,0 +1,43 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import numpy as np 
+import pandas as pd 
+
+# %%
+train_dir = Path('E:\data\RSNA2024')
+
+
+# %%
+class CFG:
+    random_seed = 42
+    
+    ROOT_FOLDER = train_dir
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
